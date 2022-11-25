56bf1e7bf9c2528dffc56be0075863d6393a7850
CsmcUsers <58969281+CsmcUsers@users.noreply.github.com>
2022-11-25 13:28:25 +0800

    Update withValueField.js

--- src/hoc/select/withValueField.js
+++ src/hoc/select/withValueField.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { DropDownList } from '@progress/kendo-react-dropdowns';
-import { filterBy } from '@progress/kendo-data-query';
 
 const isPresent = (value) => value !== null && value !== undefined;
 
@@ -52,72 +51,3 @@ export default function withValueField(DropDownComponent) {
         }
     };
 }
-
-const withFilter =
-    (WrappedComponent) =>
-    ({ delay = 300, minLen = 0, allData = [], ...props }) => {
-        const timeout = React.useRef(false);
-
-        const [state, setState] = useState({
-            data: [],
-            loading: false,
-        });
-
-        const filterData = (filter) => {
-            const fData = allData.slice();
-            return filterBy(fData, filter);
-        };
-
-        const filterChange = (event) => {
-            clearTimeout(timeout.current);
-
-            if (event.filter.value.length < minLen) {
-                setState({
-                    data: allData.slice(),
-                });
-                return;
-            }
-
-            timeout.current = setTimeout(() => {
-                setState({
-                    loading: false,
-                    data: filterData(event.filter),
-                });
-            }, delay);
-
-            setState({ ...state, loading: true });
-        };
-
-        useEffect(() => {
-            setState({ ...state, data: allData.slice() });
-        }, [allData]);
-
-        return (
-            <>
-                <WrappedComponent
-                    data={state.data}
-                    onFilterChange={filterChange}
-                    filterable={true}
-                    textField='text'
-                    valueField='id'
-                    loading={state.loading}
-                    {...props}
-                ></WrappedComponent>
-            </>
-        );
-    };
-
-/**
- * 下面 textField 和 valueField 一定要輸入
- * @param textField
- * @param valueField
- * @param data
- */
-export const DLvalue = withValueField(DropDownList);
-
-/**
- * 有過濾功能的下拉選單
- * @param textField  text
- * @param valueField id
- */
-export const DDLWithFilter = withFilter(withValueField(DropDownList));
